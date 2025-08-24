e9dcbfd2b059a867a5636194cbbff58c03ec2428
Роман Разумный <Kwentin3@gmail.com>
2025-08-24 10:22:06 +0000

    studio-1399218407:~/studio{main}$ npm run db:migrate
    > nextn@0.1.0 db:m

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE!,
+    ssl: 'allow', // allow self-signed certificates
   },
   verbose: true,
   strict: true,
