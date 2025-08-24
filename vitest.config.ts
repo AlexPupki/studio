c2ea130cdfaa2aa1314922c8a67e7117e260e51a
Роман Разумный <Kwentin3@gmail.com>
2025-08-24 09:33:23 +0000

    > nextn@0.1.0 test > vitest run
    The CJS build of Vite's Node API is dep

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    setupFiles: './tests/setup.ts',
+    exclude: [
+        '**/node_modules/**', 
+        '**/dist/**', 
+        '**/cypress/**', 
+        '**/.{idea,git,cache,output,temp}/**', 
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        'tests/e2e/**' // Exclude Playwright tests
+    ],
   },
   resolve: {
     alias: {
