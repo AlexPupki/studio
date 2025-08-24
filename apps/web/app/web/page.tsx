74582d02a24838e7b16916e27b73eca4a6ed2985
Роман Разумный <Kwentin3@gmail.com>
2025-08-24 15:36:14 +0000

    давай побудем в режиме обсуждения. и давай поищим дубли файлов или папок

--- apps/web/app/web/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Hello from App Router</h1>
-      <p>The Next.js application is running.</p>
-    </main>
-  );
-}
